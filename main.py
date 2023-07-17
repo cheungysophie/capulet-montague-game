2ca76c63668c6f81ff89b12ce93f76d3f18118a0
cheungysophie <80649756+cheungysophie@users.noreply.github.com>
2023-07-16 21:31:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ from getpass import getpass as input
 
 print("epic battle between capulet and montague")
 print("pick between R, P or S")
-capulet = input("select your move, capulet: ")
-montague = input("select your move, montague: ")
 
-if capulet == "R" and montague == "S":
-  print("aaaanddddd capulet wins this round!")
-elif capulet == "R" and montague == "R":
-  print("and all families survive! tis a tie")
-elif capulet == "R" and montague == "P":
-  print("aaandddd montague strikes win")
-elif capulet == "P" and montague == "S":
-  print("aaannnddd montague cuts the paper! congrats montague")
-elif capulet == "P" and montague == "P":
-  print("looks like both families survive and need a shirt because tis a tie!")
-elif capulet == "S" and montague == "S":
-  print("we may need another fight because tis another draw!")
-else:
-  print("hmm looks like something is wrong! check if you used 'R', 'P', or 'S'!")
+capScore = 0
+monScore = 0
+
+while True:
+  if capScore == 3:
+    print("cap win")
+    break
+  elif monScore == 3:
+    print("mon win")
+    break
+  else:
+    capulet = input("select your move, capulet: ")
+    montague = input("select your move, montague: ")
+    if (capulet == "R" and montague == "S") or (capulet == "P" and montague == "R") or (capulet == "S" and montague == "P"):
+      print("aanddddd capulet wins this round!")
+      capScore += 1
+      continue
+    elif (capulet == "R" and montague == "P") or (capulet == "P" and montague == "S") or (capulet == "S" and montague == "R"):
+      print("andddd montague strikes win")
+      monScore += 1
+      continue
+    elif (capulet == "P" and montague == "P") or (capulet == "S" and montague == "S") or (capulet == "R" and montague == "R"):
+      print("looks like both families survive and need a shirt because tis a tie!")
+      continue
+    else:
+      print("hmm something is wrong! check if you used 'R', 'P', or 'S'!")
+      continue 
+print("lets hope the star-crossed lovers get to reunite in another world")
